5463bf32b03e5b40d7bdbafff477184abc0af95a
Matthew Grant <amattwgrant@gmail.com>
2020-06-11 16:28:18 -0400

    started array and began first function

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-This is my first project for my Full Stack JavaScript Tech Degree. This is a random quote generator.
+This is my first project for my Full Stack JavaScript Tech Degree. This is a random quote generator. With the click of a button, it will reveal a new, randomly generated quote on the page.
--- js/script.js
+++ js/script.js
@@ -10,19 +10,47 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+let newQuote = '';
 
+let quotes = [
+	{quote: `If you're not first, you're last.`, source: `Ricky Bobby`, citation: `Talladega Nights`, year: `2006`},
+	{quote: `You miss 100% of the shots you don't take. -Wayne Gretzky`, source: `Michael Scott`},
+	{quote: `Go home bugs, go to your mommy!`, source: `My friends' two year old daughter`},
+	{quote: `Get your facts first, then you can distort them as you please.`, source: `Mark Twain` },
+	{quote: `What's another word for Thesaurus?`, source: `Steven Wright`},
+	{quote: `I refuse to join any club that would have me as a memeber.`, source: `Groucho Marx` }
+];
 
-
+// citation, year, etc
 /***
  * `getRandomQuote` function
 ***/
-
-
+function getRandomQuote() {
+	// for ( let i = 0; i < quotes.length; i++ ) {
+		newQuote = `<p class="quote">${quotes[Math.floor(Math.random() * quotes.length)].quote}</p>`;
+		newQuote += `<p class="source">${quotes.source}</p>`;
+		if ( quotes.citation === true && quotes.year === true ) {
+			newQuote += `<span class="citation">${newQuote.citation}</span>`;
+			newQuote += `<span class="year">${newQuote.year}</span>`;
+		} else if ( quotes.citation === true ) {
+			newQuote += `<span class="citation">${newQuote.citation}</span>`;
+		} else if ( quotes.year === true ) {
+		newQuote += `<span class="year">${newQuote.year}</span>`;
+		
+	}
+		
+	// }
+	return newQuote;
+}
+document.getElementById('quote-box').innerHTML = (getRandomQuote());
 
 /***
  * `printQuote` function
 ***/
 
+// function printQuote() {
+
+// }
 
 
 /***
@@ -30,4 +58,4 @@ project 1 - A Random Quote Generator
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+// document.getElementById('load-quote').addEventListener("click", printQuote, false);
